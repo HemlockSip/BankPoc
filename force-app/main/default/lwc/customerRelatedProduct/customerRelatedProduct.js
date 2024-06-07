42ef92160a15d7c319aa1ade67b78d4980ae7604
Davide Tansini <davide.tansini87@gmail.com>
2024-06-08 00:32:27 +0200

    working component and starting rework to enable the RestResource

--- force-app/main/default/lwc/customerRelatedProduct/customerRelatedProduct.js
+++ force-app/main/default/lwc/customerRelatedProduct/customerRelatedProduct.js
@@ -1,36 +1,62 @@
 import { LightningElement, api, wire } from 'lwc';
-import getRelatedContact from '@salesforce/apex/CustomerRelatedProductController.getRelatedContact';
-import getRelatedContactProduct from '@salesforce/apex/CustomerRelatedProductController.getRelatedContactProduct';
 import { getRecord } from 'lightning/uiRecordApi';
+import getCustomerProductComponent from '@salesforce/apex/CustomerRelatedProductController.getCustomerProductComponent';
 
-export default class CustomerRelatedProduct extends LightningElement {
-    @api recordId;
-    contact;
-    product;
-    error;
+const FIELDS = ['Case.ContactId'];
+const PRODUCT_FIELDS = ['Product2.Name', 'Product2.Home_Country__c'];
 
-    @wire(getRecord, { recordId: '$recordId', fields: ['Case.ContactId'] })
-    wiredCase({ error, data }) {
+export default class CustomerProduct extends LightningElement {
+    @api recordId; // This is the Case record ID
+
+    contactId;
+    currency;
+    Home_Country__c;
+
+    // Get the ContactId from the Case record
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    caseRecord({ error, data }) {
         if (data) {
-            this.fetchRelatedContact(data.fields.ContactId.value);
+            this.contactId = data.fields.ContactId.value;
         } else if (error) {
-            this.error = 'Contact error is '+error;
-            console.error(error);
+            console.error('Error fetching Case record:', error);
         }
     }
 
-    fetchRelatedContact(contactId) {
-        getRelatedContact({ caseId: this.recordId })
-            .then(result => {
-                this.contact = result;
-                return getRelatedContactProduct({ homeCountry: result.Home_Country__c, productId: result.Product__c });
-            })
-            .then(result => {
-                this.product = result;
-            })
-            .catch(error => {
-                this.error = 'product error is '+error;
-                console.error(error);
-            });
+    product;
+    error;
+
+    // Fetch the Product2 data using the ContactId
+    @wire(getCustomerProductComponent, { contactId: '$contactId' })
+    wiredProduct({ error, data }) {
+        if (data) {
+            this.product = data;
+            console.log('Product:', this.product);
+            const homeCountry = this.product.Home_Country__c;
+            console.log('data e country:', data.Home_Country__c);
+            if (homeCountry) {
+                this.currency = this.getCurrency(homeCountry);
+            } else {
+                this.currency = 'Unknown'; // Default or fallback value
+            }
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.product = undefined;
+        }
     }
+
+    get hasProduct() {
+        return this.product !== undefined;
+    }
+
+    getCurrency(homeCountry){
+        if (homeCountry == 'UK'){
+            return 'GBP';
+        }
+        else{
+            return 'EUR';
+        }
+    }
+
+
 }
